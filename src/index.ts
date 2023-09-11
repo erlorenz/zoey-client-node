7f52e93f6d60239b1b024f18bc965de217009309
Erik Lorenz <erik@eriklorenz.dev>
2023-09-10 23:27:53 -0500

    Edited the exports

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,4 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-type Config = {
-  auth: {
-    consumerKey: string;
-    consumerSecret: string;
-    accessToken: string;
-    tokenSecret: string;
-  };
-  siteUrl: string;
-};
-
-class Client {
-  #auth: Config["auth"];
-  #siteUrl: string;
-
-  constructor(config: Config) {
-    this.#validateConfig(config);
-    this.#auth = config.auth;
-    this.#siteUrl = config.siteUrl;
-  }
-
-  #validateConfig(cfg: Config) {
-    const missing: string[] = [];
-    for (let [k, v] of Object.entries(cfg)) {
-      if (!v) missing.push(k);
-    }
-
-    if (missing.length)
-      throw new Error(`empty config values: ${missing.join(", ")}`);
-  }
-}
-
-const config: Config = {
-  siteUrl: process.env.ZOEY_SITE_URL!,
-  auth: {
-    consumerKey: process.env.ZOEY_CONSUMER_KEY!,
-    consumerSecret: process.env.ZOEY_CONSUMER_SECRET!,
-    accessToken: process.env.ZOEY_ACCESS_TOKEN!,
-    tokenSecret: process.env.ZOEY_TOKEN_SECRET!,
-  },
-};
-
-new Client(config);
+export { Client, Config } from "./core/client";
---
+++ src/test.ts
@@ -0,0 +1,13 @@
+import { Client } from "./index";
+
+const config = {
+  siteUrl: process.env.ZOEY_SITE_URL!,
+  auth: {
+    consumerKey: process.env.ZOEY_CONSUMER_KEY!,
+    consumerSecret: process.env.ZOEY_CONSUMER_SECRET!,
+    accessToken: process.env.ZOEY_ACCESS_TOKEN!,
+    tokenSecret: process.env.ZOEY_TOKEN_SECRET!,
+  },
+};
+
+new Client(config);
