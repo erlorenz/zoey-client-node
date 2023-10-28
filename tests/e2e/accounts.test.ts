17b224f1ca43f1bbc55a7c27b45d651bab8b51d8
Erik Lorenz <erik@eriklorenz.dev>
2023-10-28 16:37:46 -0500

    Removed some dependencies

--- tests/e2e/accounts.test.ts
+++ tests/e2e/accounts.test.ts
@@ -1,7 +1,7 @@
 import { describe, test, assert } from "vitest";
 import { ZoeyClient } from "../../src/index.js";
 import { testConfig } from "./test-config.js";
-import fs from "node:fs/promises";
+// import fs from "node:fs/promises";
 
 const zoey = new ZoeyClient(testConfig);
 
@@ -21,18 +21,17 @@ describe("accounts service", () => {
     accountId = result.data[0]!.id;
   });
 
-  test.skip("retrieves account by id", async () => {
+  test("retrieves account by id", async () => {
     const result = await zoey.accounts.retrieve(accountId);
 
     assert(result.ok, "expected result.ok to be true");
   });
 
-  test("write to file", async () => {
-    const result = await zoey.client.makeRequest({
-      path: zoey.accounts.resourcePath + "/list",
-      queryParams: { limit: "1", page: "1" },
-    });
-    assert(result.ok);
-    await fs.writeFile("mocks/accounts.json", JSON.stringify(result.data));
-  });
+  // test("write to file", async () => {
+  //   const result = await zoey.client.makeRequest({
+  //     path: zoey.accounts.resourcePath + "/list",
+  //     queryParams: { limit: "1", page: "1" },
+  //   });
+  //   assert(result.ok);
+  // });
 });
